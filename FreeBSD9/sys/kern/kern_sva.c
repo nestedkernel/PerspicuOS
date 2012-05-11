4801930d3e317fb779cd426997d63ce5184ccb03
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-05-11 04:38:52 +0000

    Added comments. Added an implementation of svaReleaseMemory().
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10210 42e9315a-1956-0410-9e23-90ee2fe044bf

--- FreeBSD9/sys/kern/kern_sva.c
+++ FreeBSD9/sys/kern/kern_sva.c
@@ -15,8 +15,37 @@
 #include <sys/malloc.h>
 #include <sys/types.h>
 
+/*
+ * Function: provideSVAMemory()
+ *
+ * Description:
+ *  Allocate memory and pass it to SVA to use.
+ *
+ * Inputs:
+ *  The amount of memory to give SVA in bytes.
+ *
+ * Return value:
+ *  The first virtual address of the memory that SVA can use.
+ */
 void *
 provideSVAMemory (uintptr_t size)
 {
 	return malloc (size);
+}
+
+/*
+ * Function: releaseSVAMemory()
+ *
+ * Description:
+ *  SVA calls this function when it no longer needs a piece of memory.
+ *
+ * Inputs:
+ *  p    - The first virtual address of the memory to release back to the OS.
+ *  size - The length of the memory in bytes to release.
+ *
+ */
+void
+releaseSVAMemory (void * p, uintptr_t size)
+{
+	return free (p);
 }
