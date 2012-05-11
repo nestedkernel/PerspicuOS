596ac436687be4fc95429df6b8044753fd3257f4
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-05-11 22:29:57 +0000

    Added a configuration file for SVA. Right now, we just need it for configuring the number of processors we'll support in an SMP system.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10214 42e9315a-1956-0410-9e23-90ee2fe044bf

---
+++ SVA/include/sva/config.h
@@ -0,0 +1,21 @@
+/*===- config.h - SVA Utilities --------------------------------------------===
+ * 
+ *                     The LLVM Compiler Infrastructure
+ *
+ * This file was developed by the LLVM research group and is distributed under
+ * the University of Illinois Open Source License. See LICENSE.TXT for details.
+ * 
+ *===----------------------------------------------------------------------===
+ *
+ * This header file contains macros that can be used to configure the SVA
+ * Execution Engine.
+ *
+ *===----------------------------------------------------------------------===
+ */
+
+#ifndef _SVA_CONFIG_H
+#define _SVA_CONFIG_H
+
+/* Total number of processors supported by this SVA Execution Engine */
+const unsigned char numProcessors=4;
+#endif
