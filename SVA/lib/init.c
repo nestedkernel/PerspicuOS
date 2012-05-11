7569291572a2160e3c8610d4edf9b7db940db71c
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-05-11 22:26:11 +0000

    Added functionality to replace the IDT, but that may not be wise at this point.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10213 42e9315a-1956-0410-9e23-90ee2fe044bf

--- SVA/lib/init.c
+++ SVA/lib/init.c
@@ -13,9 +13,83 @@
  *===----------------------------------------------------------------------===
  */
 
+/*-
+ * Copyright (c) 1989, 1990 William F. Jolitz
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *  from: @(#)segments.h  7.1 (Berkeley) 5/9/91
+ * $FreeBSD: release/9.0.0/sys/amd64/include/segments.h 227946 2011-11-24 18:44:14Z rstone $
+ */
+
+/*-
+ * Copyright (c) 2003 Peter Wemm.
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD: release/9.0.0/sys/amd64/include/cpufunc.h 223796 2011-07-05 18:42:10Z jkim $
+ */
+
 #if 0
 #include "sva/interrupt.h"
 #endif
+
+#include <string.h>
+
+#include <sys/types.h>
+
+extern int printf(const char *, ...);
 
 #if 0
 void register_x86_interrupt (int number, void *interrupt, unsigned char priv);
@@ -53,7 +127,7 @@ void * interrupt_table[256];
  *  This is the x86 interrupt descriptor table.  We use it to hold all of the
  *  interrupt vectors internally within the Execution Engine.
  */
-static unsigned long sva_idt[256*2];
+static unsigned long sva_idt[2*256];
 
 #if 0
 /*
@@ -156,35 +230,45 @@ fptrap (void)
  */
 static void
 init_idt (void) {
-  /* Variable holding information about the IDT */
-  char sva_idtreg[10];
+  /* Argument to lidt/sidt taken from FreeBSD. */
+  struct region_descriptor {
+    unsigned long rd_limit:16;    /* segment extent */
+    unsigned long rd_base :64 __attribute__ ((packed));  /* base address  */
+  } __attribute__ ((packed)) sva_idtreg;
 
-  /* Index variable */
-  int index;
-
-  /*
-   * Find the current interrupt descriptor table.
-   */
-  __asm__ __volatile__ ("sidt %0": "=m" (sva_idtreg[0]));
 #if 0
   /*
    * Set up the table in memory.  Each entry will be an interrupt gate to
    * a dummy function.
    */
-  for (index = 0; index < 256; index++)
+  for (int index = 0; index < 256; index++)
   {
     interrupt_table[index] =  default_interrupt;
   }
 #endif
 
-#if 0
   /*
-   * Now tell the hardware where the table is.
+   * Find the current interrupt descriptor table (IDT).
    */
-  *(unsigned short *)(&(sva_idtreg[2])) = (sizeof(sva_idt));
-  *(unsigned int *)(&(sva_idtreg[4])) = &(sva_idt[0]);
-  __asm__ __volatile__ ("lidt %0": "=m" (sva_idtreg[2]));
+  __asm__ __volatile__ ("sidt %0": "=m" (sva_idtreg));
+
+  printf ("SVA: %x %lx\n", sva_idtreg.rd_limit, sva_idtreg.rd_base);
+
+  /*
+   * Copy the contents of the old IDT into the SVA IDT.
+   */
+  unsigned short copySize = sva_idtreg.rd_limit + 1;
+  memcpy (sva_idt, (unsigned char *) sva_idtreg.rd_base, copySize);
+
+  /*
+   * Load our descriptor table on to the processor.
+   */
+#if 1
+  sva_idtreg.rd_limit = sizeof (sva_idt);
+  sva_idtreg.rd_base = (uintptr_t) sva_idt;
+  __asm__ __volatile__ ("lidt (%0)" : : "r" (&sva_idtreg));
 #endif
+  return;
 }
 
 #if 0
