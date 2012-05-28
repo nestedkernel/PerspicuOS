f018a9190be379fcadd126a3b36bac62cc77ccd4
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-05-28 15:44:18 +0000

    Fixed the array indexing order of number of processors and IDT entries so that IDT entries are consecutive in memory. Modified registration of IDT entries so that everything uses a single table.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10262 42e9315a-1956-0410-9e23-90ee2fe044bf

--- SVA/lib/init.c
+++ SVA/lib/init.c
@@ -153,7 +153,7 @@ struct  gate_descriptor {
  *
  *  Note that we need one of these per processor.
  */
-static struct gate_descriptor sva_idt[256][numProcessors];
+static struct gate_descriptor sva_idt[numProcessors][256];
 
 /* Taken from segments.h in FreeBSD */
 static const unsigned int SDT_SYSIGT=14;  /* system 64 bit interrupt gate */
@@ -185,14 +185,18 @@ register_x86_interrupt (int number, void *interrupt, unsigned char priv) {
   /*
    * First determine which interrupt table we should be modifying.
    */
+#if 1
   unsigned int procID = getProcessorID();
-  struct gate_descriptor *ip = &sva_idt[number][procID];
+#else
+  unsigned int procID = 0;
+#endif
+  struct gate_descriptor *ip = &sva_idt[procID][number];
 
   /*
    * Add the entry into the table.
    */
   ip->gd_looffset = (uintptr_t)interrupt;
-  ip->gd_selector = GSEL(GCODE_SEL, priv);
+  ip->gd_selector = GSEL(GCODE_SEL, 0);
   ip->gd_ist = 0;
   ip->gd_xx = 0;
   ip->gd_type = SDT_SYSIGT;
@@ -220,7 +224,7 @@ register_x86_trap (int number, void *trap) {
    * First determine which interrupt table we should be modifying.
    */
   unsigned int procID = getProcessorID();
-  struct gate_descriptor *ip = &sva_idt[number][procID];
+  struct gate_descriptor *ip = &sva_idt[procID][number];
 
   /*
    * Add the entry into the table.
@@ -299,7 +303,7 @@ init_idt (unsigned int procID) {
    * Load our descriptor table on to the processor.
    */
   sva_idtreg.rd_limit = sizeof (struct gate_descriptor[256]) - 1;
-  sva_idtreg.rd_base = (uintptr_t) &(sva_idt[0][procID]);
+  sva_idtreg.rd_base = (uintptr_t) &(sva_idt[procID][0]);
   __asm__ __volatile__ ("lidt (%0)" : : "r" (&sva_idtreg));
   idt = (void *) sva_idtreg.rd_base;
 
