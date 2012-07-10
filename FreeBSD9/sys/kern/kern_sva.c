372a4ec6428b4cd5ed291e73fe2ead54d8cc8ee7
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-07-10 22:26:09 +0000

    Modified the provideSecureMemory() function in the FreeBSD kernel so that it actually allocates memory.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10306 42e9315a-1956-0410-9e23-90ee2fe044bf

--- FreeBSD9/sys/kern/kern_sva.c
+++ FreeBSD9/sys/kern/kern_sva.c
@@ -12,8 +12,26 @@
  *===----------------------------------------------------------------------===
  */
 
+#if 0
+#include "opt_pmap.h"
+#include "opt_vm.h"
+#endif
+
 #include <sys/malloc.h>
 #include <sys/types.h>
+
+#include <sys/cdefs.h>
+#include <vm/vm.h>
+#include <vm/vm_param.h>
+#include <vm/vm_kern.h>
+#include <vm/vm_page.h>
+#include <vm/vm_map.h>
+#include <vm/vm_object.h>
+#include <vm/vm_extern.h>
+#include <vm/vm_pageout.h>
+#include <vm/vm_reserv.h>
+#include <vm/pmap.h>
+#include <vm/uma.h>
 
 /* Function prototypes */
 void * provideSVAMemory (uintptr_t size);
@@ -31,12 +49,34 @@ void releaseSVAMemory (void * p, uintptr_t size);
  * Return value:
  *  The first virtual address of the memory that SVA can use.
  */
+static char buffer[4096] __attribute__ ((aligned (4096)));
+
 void *
 provideSVAMemory (uintptr_t size)
 {
-  static char buffer[4096];
-  void * p = buffer;
-  printf ("SVA: providesSVAMemory: %p %ld\n", p, size);
+  /* Structure to get a page */
+  vm_page_t bufferPage;
+
+  /* Virtual address of memory to be returned. */
+  void * p;
+
+  /*
+   * Check to see if a single page will do.  If not, then panic.
+   */
+  if (size > 4096u)
+    panic ("SVA: providesSVAMemory: Too much memory requested!\n");
+
+  /*
+   * Request a page from the page manager.
+   */
+	bufferPage = vm_page_alloc (NULL, 0, VM_ALLOC_NORMAL | VM_ALLOC_WIRED);
+
+  /*
+   * Convert the page into a physical address.
+   */
+	p = (void *) PHYS_TO_DMAP(VM_PAGE_TO_PHYS(bufferPage));
+
+  printf ("SVA: providesSVAMemory: %p -> %lx\n", p, vtophys (p));
 	return p;
 }
 
