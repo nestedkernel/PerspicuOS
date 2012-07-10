073158815ea7b4ad01184ff4177c55d03c5669c5
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-07-10 19:37:14 +0000

    Modified the code to try to read from secure memory first.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10301 42e9315a-1956-0410-9e23-90ee2fe044bf

--- apps/test/test.c
+++ apps/test/test.c
@@ -2,9 +2,24 @@
 #include <stdlib.h>
 
 int
-main () {
-  void * ptr;
-  __asm__ __volatile__ ("movq $8, %%rdi\nint $0x7f\n" : "=a" (ptr));
-  printf ("%p\n", ptr);
+main (int argc, char ** argv) {
+  unsigned char * ptr = 0;
+  unsigned long size = 0;
+  unsigned index;
+  unsigned sum = 0;
+  size = strtoul (argv[1], NULL, 0);
+  __asm__ __volatile__ ("movq %1, %%rdi\nint $0x7f\n" : "=a" (ptr) : "r" (size));
+  __asm__ __volatile__ ("movq %1, %%rdi\nint $0x7f\n" : "=a" (ptr) : "r" (size));
+  printf ("Secure Memory at %p\n", (void *)ptr);
+
+  printf ("Reading Secure Memory at %p\n", (void *)ptr);
+  fflush (stdout);
+  for (index = 0; index < size; ++index) {
+    printf ("Address: %p\n", &(ptr[index]));
+    fflush (stdout);
+    sum += ptr[index];
+  }
+  printf ("%d\n", sum);
+
   return 0;
 }
