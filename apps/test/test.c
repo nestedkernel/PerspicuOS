a88538c29040e24492a8e01bbd160cbfc00a75bf
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-07-11 15:56:05 +0000

    Added comments. Added code to write to the secure memory. Added code to free the secure memory.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10310 42e9315a-1956-0410-9e23-90ee2fe044bf

--- apps/test/test.c
+++ apps/test/test.c
@@ -7,11 +7,23 @@ main (int argc, char ** argv) {
   unsigned long size = 0;
   unsigned index;
   unsigned sum = 0;
+
+  /*
+   * Get the number of bytes of secure memory to allocate.
+   */
   size = strtoul (argv[1], NULL, 0);
-  __asm__ __volatile__ ("movq %1, %%rdi\nint $0x7f\n" : "=a" (ptr) : "r" (size));
+  printf ("Secure Memory size: %lx\n", size);
+
+  /*
+   * Call the secure memory allocator.
+   */
   __asm__ __volatile__ ("movq %1, %%rdi\nint $0x7f\n" : "=a" (ptr) : "r" (size));
   printf ("Secure Memory at %p\n", (void *)ptr);
 
+
+  /*
+   * Read from the secure memory.
+   */
   printf ("Reading Secure Memory at %p\n", (void *)ptr);
   fflush (stdout);
   for (index = 0; index < size; ++index) {
@@ -21,5 +33,26 @@ main (int argc, char ** argv) {
   }
   printf ("%d\n", sum);
 
+  /*
+   * Write to the secure memory.
+   */
+  printf ("Writing Secure Memory at %p\n", (void *)ptr);
+  fflush (stdout);
+  for (index = 0; index < size; ++index) {
+    printf ("Address: %p\n", &(ptr[index]));
+    ptr[index] = 'c';
+  }
+  for (index = 0; index < size; ++index) {
+    printf ("Address: %p %c\n", &(ptr[index]), ptr[index]);
+  }
+
+  /*
+   * Free the secure memory.
+   */
+  printf ("Freeing Secure Memory at %p\n", (void *)ptr);
+  fflush (stdout);
+  __asm__ __volatile__ ("int $0x7e\n" : : "D" (ptr), "S" (size));
+
+  printf ("Done!\n");
   return 0;
 }
