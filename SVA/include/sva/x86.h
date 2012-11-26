fa231511fa40b06dcdf8cfae21bbf4d14ee6c6a2
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-11-26 20:12:12 +0000

    Added a header file describing the x86.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10807 42e9315a-1956-0410-9e23-90ee2fe044bf

---
+++ SVA/include/sva/x86.h
@@ -0,0 +1,54 @@
+/*===- x86.h - SVA Execution Engine ----------------------------------------===
+ *
+ *                        Secure Virtual Architecture
+ *
+ * This file was developed by the LLVM research group and is distributed under
+ * the University of Illinois Open Source License. See LICENSE.TXT for details.
+ *
+ *===----------------------------------------------------------------------===
+ *
+ * This file defines structures used by the x86_64 architecture.
+ *
+ *===----------------------------------------------------------------------===
+ */
+
+#include <sys/types.h>
+
+/*
+ * Struction: tss_t
+ *
+ * Description:
+ *  This is an x86_64 Task State Segment.
+ */
+typedef struct {
+  unsigned reserved0 __attribute__((packed));
+
+  /*
+   * Pointers to the kernel stack pointer when the interrupt stack table is not
+   * used
+   */
+  uintptr_t rsp0 __attribute__((packed));
+  uintptr_t rsp1 __attribute__((packed));
+  uintptr_t rsp2 __attribute__((packed));
+  uintptr_t reserved1 __attribute__((packed));
+
+  /*
+   * Interrupt Stack Table (IST) Pointers: Marks where the kernel stack should
+   * be set on interrupt.
+   */
+  uintptr_t ist1 __attribute__((packed));
+  uintptr_t ist2 __attribute__((packed));
+  uintptr_t ist3 __attribute__((packed));
+  uintptr_t ist4 __attribute__((packed));
+  uintptr_t ist5 __attribute__((packed));
+  uintptr_t ist6 __attribute__((packed));
+  uintptr_t ist7 __attribute__((packed));
+
+  uintptr_t reserved2 __attribute__((packed));
+  uintptr_t reserved3 __attribute__((packed));
+
+  /* I/O Permission Map */
+  unsigned int iomap __attribute__((packed));
+} tss_t;
+
+
