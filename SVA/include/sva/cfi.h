954312fa027c8e0e327cd4659232703fc652741a
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2012-12-06 22:29:20 +0000

    Added header file for manually adding CFI labels to assembly code.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@10832 42e9315a-1956-0410-9e23-90ee2fe044bf

---
+++ SVA/include/sva/cfi.h
@@ -0,0 +1,42 @@
+/*===- cfi.h - SVA Execution Engine  =-------------------------------------===
+ * 
+ *                        Secure Virtual Architecture
+ *
+ * This file was developed by the LLVM research group and is distributed under
+ * the University of Illinois Open Source License. See LICENSE.TXT for details.
+ * 
+ *===----------------------------------------------------------------------===
+ *
+ * This file defines macros that can be used to add CFI checks and labels to
+ * hand-written assembly code.
+ *
+ *===----------------------------------------------------------------------===
+ */
+
+#ifndef SVA_CFI_H
+#define SVA_CFI_H
+
+/* Labels for call targets and return targets, respectively */
+#define CALLLABEL 0xbeefbeef
+#define RETLABEL  0xbeefbeef
+
+/* Labels used in comparisons: This includes the prefetchnta portion */
+#define CHECKLABEL 0x80180F67
+
+/* Macro for call */
+#define CALLQ(x) callq x \
+								 prefetchnta RETLABEL(%eax)
+
+/* Macro for start of function */
+#define STARTFUNC prefetchnta CALLLABEL(%eax)
+
+/* Macro for return */
+#define RETQ  movq  (%rsp), %rcx ; \
+              addq  $8, %rsp ; \
+              cmpl  $CHECKLABEL, (%rcx) ; \
+							jne 1f ; \
+							jmpq  *%rcx ; \
+							1: movq $0x0fea, %rcx; \
+							jmpq  *%rcx ; \
+
+#endif
