2a1f3783b9d57ddfab1c4f7e8b85c448e2f6e361
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-02-07 02:06:33 +0000

    Simplified init application (called panic) used for testing.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11028 42e9315a-1956-0410-9e23-90ee2fe044bf

---
+++ apps/init/panic2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+char * msg = "Hello SVA!\n";
+
+int
+main (int argc, char ** argv) {
+  int fd;
+  int pid;
+  static char * msg = "SVA: child: I can do stuff!\n";
+
+  __asm__ __volatile__ ("int $0x7e\n");
+  fd = open ("/tmp/log2", O_WRONLY | O_CREAT | O_TRUNC | O_FSYNC, 0644);
+  write (fd, msg, strlen(msg));
+  close (fd);
+
+  while (1) { ; }
+  return 0;
+}
+
