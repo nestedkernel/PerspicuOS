94a8ff691a193c9a55f95b05541ffb9946cff0af
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-02-07 02:08:09 +0000

    Permit the initialization of one of the FPU PCB fields.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11029 42e9315a-1956-0410-9e23-90ee2fe044bf

--- FreeBSD9/sys/amd64/amd64/machdep.c
+++ FreeBSD9/sys/amd64/amd64/machdep.c
@@ -892,7 +892,9 @@ exec_setregs(struct thread *td, struct image_params *imgp, u_long stack)
 	pcb->pcb_fsbase = 0;
 	pcb->pcb_gsbase = 0;
 	clear_pcb_flags(pcb, PCB_32BIT | PCB_GS32BIT);
+#endif
 	pcb->pcb_initial_fpucw = __INITIAL_FPUCW__;
+#if 0
 	set_pcb_flags(pcb, PCB_FULL_IRET);
 
 	bzero((char *)regs, sizeof(struct trapframe));
