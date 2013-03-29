3b67fa721f6e4100fa3467e84a0816e9a8107bec
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-03-29 21:48:50 +0000

    Added a test for sigaction() signal handlers.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11215 42e9315a-1956-0410-9e23-90ee2fe044bf

--- apps/sigtest/sigtest.c
+++ apps/sigtest/sigtest.c
@@ -3,7 +3,10 @@
 
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/ucontext.h>
 #include <unistd.h>
+
+int pid = 0;
 
 void
 sighandler (int signal) {
@@ -12,19 +15,32 @@ sighandler (int signal) {
   return;
 }
 
+void
+sigacthandler (int signal, siginfo_t * sinfo, void * p) {
+  ucontext_t * up = p;
+  printf ("4: Caught signal %d: pid=%d, sig pid=%d, flags=%lx\n", signal, pid, sinfo->si_pid, up->uc_flags);
+}
+
 int
 main (int argc, char ** argv) {
+  struct sigaction usr2action;
+  usr2action.sa_sigaction = sigacthandler;
+  usr2action.sa_flags = SA_SIGINFO;
+
+  pid = getpid();
   printf ("1: Setting up signal handler\n");
   fflush (stdout);
 
   signal (SIGUSR1, sighandler);
+  sigaction (SIGUSR2, &usr2action, NULL);
 
   printf ("2: Sending myself the signal\n");
   fflush (stdout);
 
   kill (getpid(), SIGUSR1);
+  kill (getpid(), SIGUSR2);
 
-  printf ("4: Resuming from signal handler\n");
+  printf ("5: Resuming from signal handler\n");
   fflush (stdout);
 
   return 0;
