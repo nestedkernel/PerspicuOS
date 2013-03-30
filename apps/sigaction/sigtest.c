b9f3533658cba58e5a20629072adaf2a0758c75d
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-03-30 15:47:32 +0000

    Create a special test program just for sigaction.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11217 42e9315a-1956-0410-9e23-90ee2fe044bf

---
+++ apps/sigaction/sigtest.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/ucontext.h>
+#include <unistd.h>
+
+int pid = 0;
+
+void
+sighandler (int signal) {
+  printf ("3: Caught signal %d: %lx\n", signal, __builtin_return_address(0));
+  fflush (stdout);
+  return;
+}
+
+void
+sigacthandler (int signal, siginfo_t * sinfo, void * p) {
+  ucontext_t * up = p;
+  printf ("4: Caught signal %d: pid=%d, sig pid=%d, flags=%lx\n", signal, pid, sinfo->si_pid, up->uc_flags);
+}
+
+int
+main (int argc, char ** argv) {
+  struct sigaction usr2action;
+  usr2action.sa_sigaction = sigacthandler;
+  usr2action.sa_flags = SA_SIGINFO;
+
+  pid = getpid();
+  printf ("1: Setting up signal handler\n");
+  fflush (stdout);
+
+  signal (SIGUSR1, sighandler);
+  sigaction (SIGUSR2, &usr2action, NULL);
+
+  printf ("2: Sending myself the signal\n");
+  fflush (stdout);
+
+  kill (getpid(), SIGUSR1);
+  kill (getpid(), SIGUSR2);
+
+  printf ("5: Resuming from signal handler\n");
+  fflush (stdout);
+
+  return 0;
+}
+
