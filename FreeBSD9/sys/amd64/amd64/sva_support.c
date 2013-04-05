f554b82771cd5e6c458a9b144a4889dc17584266
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-04-05 21:27:19 +0000

    Implemented a new version of copyinstr() that uses the SVA-OS intrinsics.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11275 42e9315a-1956-0410-9e23-90ee2fe044bf

--- FreeBSD9/sys/amd64/amd64/sva_support.c
+++ FreeBSD9/sys/amd64/amd64/sva_support.c
@@ -1,6 +1,13 @@
 #include <sys/cdefs.h>
 #include <sys/param.h>
 #include <sys/systm.h>
+#include <sys/pcpu.h>
+
+#include <vm/vm.h>
+#include <vm/pmap.h>
+
+#include <machine/vmparam.h>
+#include <machine/pcb.h>
 
 /*
  * Prototypes for the real functions.
@@ -12,6 +19,53 @@ extern int real_copyout(const void * __restrict kaddr, void * __restrict udaddr,
 /*
  * Implementations of the functions that use the SVA-OS intrinsics.
  */
+int
+copyinstr(const void * __restrict udaddr, void * __restrict kaddr,
+	        size_t len, size_t * __restrict lencopied) {
+  /* Number of bytes copied */
+  uintptr_t copySize;
+
+  /*
+   * Ensure that the copy won't read in kernel-space memory for the string.
+   */
+  if (VM_MAXUSER_ADDRESS <= udaddr)
+    return EFAULT;
+
+  if (len >= (VM_MAXUSER_ADDRESS - (uintptr_t)udaddr))
+    len = (VM_MAXUSER_ADDRESS - (uintptr_t)udaddr);
+
+  /*
+   * Note that we want to use SVA to unwind the stack if we hit a fault.
+   */
+  PCPU_GET(curpcb)->pcb_onfault = 1;
+
+  /*
+   * Perform the copy.
+   */
+  copySize = sva_invokestrncpy (kaddr, udaddr, len);
+
+  /*
+   * Note that we aren't expecting a fault now.
+   */
+  PCPU_GET(curpcb)->pcb_onfault = 0;
+
+  /*
+   * Determine if the string name is too long or if we hit a fault.
+   */
+  if (copySize == -1)
+    return EFAULT;
+
+  if (copySize == len)
+    return ENAMETOOLONG;
+
+  /*
+   * Report the number of bytes copied to the caller.
+   */
+  if (lencopied)
+    *lencopied = copySize + 1;
+  return 0;
+}
+
 int
 copyin(const void * __restrict udaddr,
        void * __restrict kaddr,
