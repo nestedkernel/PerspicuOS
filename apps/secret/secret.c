04e89885c7997052b8de01b4f50aaea9bd318da6
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-04-24 02:34:14 +0000

    Made the secret program read and write on a pipe forever. Had it print out information useful for configuring the attack.
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11365 42e9315a-1956-0410-9e23-90ee2fe044bf

--- apps/secret/secret.c
+++ apps/secret/secret.c
@@ -2,13 +2,40 @@
 #include <stdlib.h>
 
 #include <unistd.h>
+#include <errno.h>
 
-volatile char * secret = "secret-jtc";
+volatile char * secret = "\nsecret-jtc";
 int
 main (int argc, char ** argv) {
+  int fds[2];
+
+  /*
+   * Create a pipe.
+   */
+  if (pipe (fds) == -1) {
+    printf ("pipe error: %d\n", errno);
+  }
+
+  printf ("%p %lx\n", secret, strlen (secret));
   printf ("PID = %d\n", getpid());
   fflush (stdout);
-  sleep (3600);
+
+  /*
+   * Read and write data on the pipe.
+   */
+  do {
+    static unsigned char buf;
+    write (fds[1], "c", 1);
+    printf ("w");
+    fflush (stdout);
+
+    read (fds[0], &buf, 1);
+    printf ("r");
+    fflush (stdout);
+
+    sleep (5);
+  } while (1);
+
   return 0;
 }
 
