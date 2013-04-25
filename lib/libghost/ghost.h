3a861f83863a1e55a7d77c890666f89a9fe27ace
criswell <criswell@42e9315a-1956-0410-9e23-90ee2fe044bf>
2013-04-25 22:12:03 +0000

    Added a prototype for ghostinit().
    git-svn-id: https://subversion.cs.illinois.edu/svn/llvm_group/sva/trunk@11401 42e9315a-1956-0410-9e23-90ee2fe044bf

--- lib/libghost/ghost.h
+++ lib/libghost/ghost.h
@@ -1,4 +1,6 @@
 //
 // Declare all of the system call wrappers as having C linkage.
 //
+extern "C" void ghostinit (void);
 extern "C" int accept (int s, struct sockaddr * addr, socklen_t * addrlen);
+extern "C" int fstat(int fd, struct stat *sb);
